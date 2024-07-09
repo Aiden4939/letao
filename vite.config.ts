3cd28d6c3fbe4a616a55e70e3ba149838c5a61ff
Aiden4939 <103229364+Aiden4939@users.noreply.github.com>
2024-07-09 21:59:03 +0800

    fixed vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  root: "src",
-  build: {
-    outDir: "../../letao_page",
-    emptyOutDir: true, // also necessary
-  },
 });
